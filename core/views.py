ab85260ae11fda48dc36838a92b892b947b1e958
danyel66 <danielonyeka247@gmail.com>
2020-12-02 14:37:02 +0100

    second commit

--- core/views.py
+++ core/views.py
@@ -16,15 +16,40 @@ from .utils import account_activation_token
 from django.urls import reverse
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+import os
+from django.conf import settings
+from .models import UserPreference
+
 
 
 @login_required(login_url='login')
 def index(request):
-    template_name='core/index.html'
-    context = {
+    currency_data = []
+    file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
 
-    }
-    return render(request, template_name, context)
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+        for k, v in data.items():
+            currency_data.append({'name': k, 'value': v})
+
+    exists = UserPreference.objects.filter(user=request.user).exists()
+    user_preferences = None
+    if exists:
+        user_preferences = UserPreference.objects.get(user=request.user)
+    if request.method == 'GET':
+
+        return render(request, 'core/index.html', {'currencies': currency_data,
+                                                          'user_preferences': user_preferences})
+    else:
+
+        currency = request.POST['currency']
+        if exists:
+            user_preferences.currency = currency
+            user_preferences.save()
+        else:
+            UserPreference.objects.create(user=request.user, currency=currency)
+        messages.success(request, 'Changes saved')
+        return render(request, 'core/index.html', {'currencies': currency_data, 'user_preferences': user_preferences})
 
 
 
